d7fbcc979955221914aa03c72f3e7a62eb29807e
SeungWoo <kangsw1025@naver.com>
2025-02-05 22:50:57 +0900

    프로젝트 생성

---
+++ MuMuDuck/MuMuDuck/App/MuMuDuckApp.swift
@@ -0,0 +1,17 @@
+//
+//  MuMuDuckApp.swift
+//  MuMuDuck
+//
+//  Created by 강승우 on 2/5/25.
+//
+
+import SwiftUI
+
+@main
+struct MuMuDuckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
