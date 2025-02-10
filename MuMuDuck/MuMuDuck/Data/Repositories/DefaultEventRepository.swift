53e3a0919e4719918c6f84da4d13d9f139bfd7d2
SeungWoo <kangsw1025@naver.com>
2025-02-11 03:45:43 +0900

    Refactor: DefaultEventRepository Mock 데이터 추가

--- MuMuDuck/MuMuDuck/Data/Repositories/DefaultEventRepository.swift
+++ MuMuDuck/MuMuDuck/Data/Repositories/DefaultEventRepository.swift
@@ -28,11 +28,13 @@ class DefaultEventRepository: EventRepository {
         }
         
         self.events = [
-            PersonalEvent(title: "테스트1", isAllDay: false, startDate: getDate(year: 2025, month: 2, day: 8), endDate: getDate(year: 2025, month: 2, day: 9), isAlert: false),
-            PersonalEvent(title: "테스트2", isAllDay: false, startDate: getDate(year: 2025, month: 2, day: 10), endDate: getDate(year: 2025, month: 2, day: 13), isAlert: false),
-            PersonalEvent(title: "테스트3", isAllDay: false, startDate: getDate(year: 2025, month: 2, day: 12), endDate: getDate(year: 2025, month: 2, day: 13), isAlert: false),
-            PersonalEvent(title: "테스트4", isAllDay: false, startDate: getDate(year: 2025, month: 2, day: 18), endDate: getDate(year: 2025, month: 2, day: 20), isAlert: false),
-            PersonalEvent(title: "테스트5", isAllDay: false, startDate: getDate(year: 2025, month: 2, day: 21), endDate: getDate(year: 2025, month: 2, day: 21), isAlert: false),
+            PersonalEvent(title: "테스트A", isAllDay: true, startDate: getDate(year: 2025, month: 02, day: 13), endDate: getDate(year: 2025, month: 02, day: 19), isAlert: true, memo: "테스트 메모A"),
+            PersonalEvent(title: "테스트B", isAllDay: false, startDate: getDate(year: 2025, month: 01, day: 27), endDate: getDate(year: 2025, month: 01, day: 30), isAlert: true, memo: "테스트 메모B"),
+            PersonalEvent(title: "테스트C", isAllDay: true, startDate: getDate(year: 2025, month: 02, day: 19), endDate: getDate(year: 2025, month: 02, day: 25), isAlert: false, memo: "테스트 메모C"),
+            PersonalEvent(title: "테스트D", isAllDay: true, startDate: getDate(year: 2025, month: 02, day: 23), endDate: getDate(year: 2025, month: 02, day: 25), isAlert: false, memo: "테스트 메모D"),
+            PersonalEvent(title: "테스트E", isAllDay: true, startDate: getDate(year: 2025, month: 02, day: 19), endDate: getDate(year: 2025, month: 02, day: 28), isAlert: false, memo: "테스트 메모E"),
+            PersonalEvent(title: "테스트F", isAllDay: true, startDate: getDate(year: 2025, month: 02, day: 12), endDate: getDate(year: 2025, month: 02, day: 23), isAlert: false, memo: "테스트 메모F"),
+            PersonalEvent(title: "테스트G", isAllDay: true, startDate: getDate(year: 2025, month: 02, day: 25), endDate: getDate(year: 2025, month: 3, day: 1), isAlert: false, memo: "테스트 메모G"),
         ]
     }
     
